02823d9cfb240b8574c4004b29bf457ed797c344
Natiqq <natiqq@gmail.com>
2016-03-10 15:49:21 +0400

    Added files via upload

--- Calculate.java
+++ Calculate.java
@@ -3,10 +3,15 @@ public class Calculate {
 
 	public static void main(String[] args) {
 		System.out.println("Calculate...");
-		int first = Integer.valueOf(args[0]);
-		int second = Integer.valueOf(args[1]);
-		int summ = first + second;
+		double first = Double.valueOf(args[0]);
+		double second = Double.valueOf(args[1]);
+		double summ = first + second;
+		double differ = first - second;
+		double multipl = first * second;
+		double division = first / second;
 		System.out.println("Summ: "+ summ);
+		System.out.println("Difference: "+ differ);
+		System.out.println("Multiplication: "+ multipl);
+		System.out.println("Division: "+ division);
 	}
-
 }
